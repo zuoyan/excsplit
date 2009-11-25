3161ba02aa09c48a231d38ed0cb0b75f5bc12b23
Changsheng Jiang <jiangzuoyan@gmail.com>
2009-11-26 02:08:31 +0800

    Fixed rename

--- excsplit_test.sh
+++ excsplit_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EXCSPLIT=./excsplit
+
 function gen_prefix_seq {
     local p=$1
     shift
@@ -10,15 +12,15 @@ function gen_prefix_seq {
 
 for k in 2 3 4; do
     for fn in "$@"; do
-        rm -f /tmp/test-langrange-*
-        echo langrange $k $fn $(gen_prefix_seq /tmp/test-langrange-split $(seq `echo $k+2|bc`))
-        langrange $k $fn $(gen_prefix_seq /tmp/test-langrange-split $(seq `echo $k+2|bc`)) || exit 1
-        echo langrange -$k /tmp/test-langrange-whole $(ls /tmp/test-langrange-split* |shuf) || exit 2
-        langrange -$k /tmp/test-langrange-whole $(ls /tmp/test-langrange-split* |shuf) || exit 2
-        if diff /tmp/test-langrange-whole $fn; then
+        rm -f /tmp/test-excsplit-*
+        echo $EXCSPLIT $k $fn $(gen_prefix_seq /tmp/test-excsplit-split $(seq `echo $k+2|bc`))
+        $EXCSPLIT $k $fn $(gen_prefix_seq /tmp/test-excsplit-split $(seq `echo $k+2|bc`)) || exit 1
+        echo $EXCSPLIT -$k /tmp/test-excsplit-whole $(ls /tmp/test-excsplit-split* |shuf) || exit 2
+        $EXCSPLIT -$k /tmp/test-excsplit-whole $(ls /tmp/test-excsplit-split* |shuf) || exit 2
+        if diff /tmp/test-excsplit-whole $fn; then
             echo "correct for file $fn $k"
         else
-            echo "correct for file $fn $k" >&2
+            echo "error for file $fn $k" >&2
             exit 3
         fi
     done
